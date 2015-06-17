8c6f3658f0410740329a60dff9fbaef406911496
Jae Lee <ksy903@gmail.com>
2015-06-17 15:11:14 -0500

    Initial commit - MAde the project

---
+++ desktop/src/com/JaeLee/splitdecision/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.JaeLee.splitdecision.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.JaeLee.splitdecision.SplitDecision;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SplitDecision(), config);
+	}
+}
