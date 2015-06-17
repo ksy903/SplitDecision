305289f94dbcf5a2737d0833921d5e671d0277c9
Jae Lee <ksy903@gmail.com>
2015-06-17 18:50:28 -0500

    Added Logo Screen and fade

--- desktop/src/com/JaeLee/splitdecision/desktop/DesktopLauncher.java
+++ desktop/src/com/JaeLee/splitdecision/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.JaeLee.splitdecision.SplitDecision;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 500;
+		config.width = 1000;
+		config.vSyncEnabled = true;
 		new LwjglApplication(new SplitDecision(), config);
 	}
 }
